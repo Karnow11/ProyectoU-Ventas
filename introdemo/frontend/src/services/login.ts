9e918399d502010743e6745341ddadf60dbb3928
benjasep <sepulvveda.benjamin@gmail.com>
2025-11-08 20:35:44 -0300

    eliminacion consoleLog random

--- introdemo/frontend/src/services/login.ts
+++ introdemo/frontend/src/services/login.ts
@@ -19,7 +19,6 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const csrf = localStorage.getItem("csrfToken");
-  console.log("AAAAAAAAAAAAA")
   console.log(config.headers)
   console.log(csrf)
   if (csrf) {
@@ -35,7 +34,6 @@ const create = async (user: Create) => {
 const login = async (credentials: Credentials) => {
     const response = await api.post("/api/login", credentials);
     const csrfToken = response.headers["x-csrf-token"];
-    console.log("BBBBBBBBBBBBBBBBB")
     console.log(response.headers)
     if (csrfToken) {
         localStorage.setItem("csrfToken", csrfToken);
