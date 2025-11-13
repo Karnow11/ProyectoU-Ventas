1cfe927090cbfb02810b6fc10ca6f950d48ea67a
Andrés Bravo <andresignaciobc@gmail.com>
2025-11-12 22:13:13 -0300

    Avances e2e

--- introdemo/e2e-tests/tests/UVentas.spec.ts
+++ introdemo/e2e-tests/tests/UVentas.spec.ts
@@ -15,7 +15,19 @@ test.describe("Post app", () => {
 
         await page.goto("/");
     });
-    test.describe("A persona can register itself", () => {
-        
+    test.describe("A persona can register and login itself", () => {
+        await registerWith(page, "abcdin", "abc@abcdin.cl" , "abc123");
+        await loginWith(page, "abcdin", "abc123");
+        await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible();
     })
+
+    test.describe("You can add a SellingPoint", () => {
+        await loginWith(page, "abc", "abc123");
+        await page.getByRole('link', { name: 'Formulario Nuevo SellingPoint' }).click();
+        await expect(page.getByRole('heading', { name: 'Formulario de ventas' })).toBeVisible();
+        await createPost("test-store", "static", "comida", "soy una descripción")
+        await expect(getByRole('link', { name: 'Nombre: test-store — #' })).toBeVisible();
+    });
+
+    
 });
--- introdemo/e2e-tests/tests/helper.ts
+++ introdemo/e2e-tests/tests/helper.ts
@@ -1,6 +1,6 @@
 import { Page } from "@playwright/test";
 
-const registerWith = async (page: Page, username: string, email: string, password: string, confirmpass: string) => {
+const registerWith = async (page: Page, username: string, email: string, password: string) => {
     await page.getByRole('button', { name: 'Create Account >' }).click()
     await page.locator('input[name="Username"]').fill(username);
     await page.locator('input[name="Mail"]').fill(email);
@@ -19,9 +19,9 @@ const createPost = async (page: Page, store_name: string, movility = "static", p
     await page.getByRole('link', { name: 'Formulario Nuevo SellingPoint' }).click()
     await page.getByRole('textbox', { name: 'Nombre de tienda' }).fill(store_name);
     if movility = "static":
-        page.getByRole('radio', { name: 'Estático' }).click()
+        await page.getByRole('radio', { name: 'Estático' }).click()
     else: 
-        page.getByRole('radio', { name: 'Móvil' }).click()
+        await page.getByRole('radio', { name: 'Móvil' }).click()
     await page.getByRole('textbox', { name: 'Tipo de producto o servicio' }).fill(product_type);
     await page.getByRole('textbox', { name: 'Descripcion' }).fill(descrip);
     await page.getByRole('button', { name: 'Enviar formulario' }).click();
